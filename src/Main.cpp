ff07c2421da936da05c51512fe65f93f945a44c0
Matthew Collins <mscollins2@wpi.edu>
2017-09-04 23:14:53 -0400

    Added offsets (parts 3-7)

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,7 +5,7 @@
 Ticker pidTimer;
 static PIDBowler*  pid[numberOfPid];
 HIDSimplePacket coms;
-float  calibrations[3] = {0,0,0};
+float  calibrations[3] = {-224,0,0};
 //float  calibrations[3] = {114,784,-10};
 
 
